1a873d178bd06b978befa4f57afa8b93e32e43de
narutoben10af <limsieian@me.com>
2021-10-31 00:32:39 +0900

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Lim Si Eian on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
